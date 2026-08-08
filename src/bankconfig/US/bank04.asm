ffc5204758d2d48ca50f21d44f7f3bd5b812c616
lilliththecreative <111915545+lilliththecreative@users.noreply.github.com>
2026-08-08 10:46:00 -0500

    Update bank04.asm

--- src/bankconfig/US/bank04.asm
+++ src/bankconfig/US/bank04.asm
@@ -365,7 +365,7 @@ LOCALEINCLUDE "flyovers.symbols.asm"
 
 .INCLUDE "unknown/C4/C43568.asm"
 
-.INCLUDE "unknown/C4/C43573.asm"
+.INCLUDE "misc/swap_raised_hp_pp_window.asm"
 
 .INCLUDE "unknown/C4/C435E4.asm"
 
