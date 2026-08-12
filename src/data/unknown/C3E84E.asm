4ae11461d19bc21c22e681b7370e63e7d07ad7de
lilliththecreative <111915545+lilliththecreative@users.noreply.github.com>
2026-08-12 13:30:53 -0500

    Update C3E84E.asm

--- src/data/unknown/C3E84E.asm
+++ src/data/unknown/C3E84E.asm
@@ -1,5 +1,5 @@
 
-;Usage unknown .Only usage seems to be at UNKNOWN_C10A85, where these values are passed via X register
+;Usage unknown .Only usage seems to be at DRAW_TALL_TEXT_TILE, where these values are passed via X register
 UNKNOWN_C3E84E:
 	.WORD $0130, $0131, $0132, $0133, $0134, $0135, $0136, $0137, $0138, $0000
 UNKNOWN_C3E862:
