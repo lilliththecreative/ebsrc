11fd9ddb9c10b8369941e1a77dab272eb2007212
lilliththecreative <111915545+lilliththecreative@users.noreply.github.com>
2026-08-13 20:51:06 -0500

    Update bank00.asm

--- src/bankconfig/JP/bank00.asm
+++ src/bankconfig/JP/bank00.asm
@@ -206,7 +206,7 @@
 
 .INCLUDE "overworld/handle_normal_movement.asm"
 
-.INCLUDE "overworld/move_camera_to_entity.asm"
+.INCLUDE "overworld/camera/move_camera_to_entity.asm"
 
 .INCLUDE "overworld/handle_escalator_movement.asm"
 
