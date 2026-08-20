e692c6566399f4bd96e95d7e80a69638bf7a0d61
lilliththecreative <111915545+lilliththecreative@users.noreply.github.com>
2026-08-20 18:20:03 -0500

    Update C0369B.asm

--- src/unknown/C0/C0369B.asm
+++ src/unknown/C0/C0369B.asm
@@ -131,7 +131,7 @@ UNKNOWN_C0369B:
 	LDA @LOCAL02
 	OPTIMIZED_MULT @VIRTUAL04, .SIZEOF(character_initial_entity_entry)
 	CLC
-	ADC #character_initial_entity_entry::unknown6
+	ADC #character_initial_entity_entry::initial_entity_slot
 	TAX
 	LDA f:CHARACTER_INITIAL_ENTITY_DATA,X
 	STA @LOCAL06
@@ -273,7 +273,7 @@ UNKNOWN_C0369B:
 	DEC
 	OPTIMIZED_MULT @VIRTUAL04, .SIZEOF(character_initial_entity_entry)
 	CLC
-	ADC #character_initial_entity_entry::unknown6
+	ADC #character_initial_entity_entry::initial_entity_slot
 	CLC
 	ADC @VIRTUAL06
 	STA @VIRTUAL06
