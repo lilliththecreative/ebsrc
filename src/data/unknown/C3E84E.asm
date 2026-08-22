15966ae337c153b0900445f7f7b4f9bd69db2db1
lilliththecreative <111915545+lilliththecreative@users.noreply.github.com>
2026-08-21 19:14:46 -0500

    Update and rename C3E84E.asm to smaaaash_tiles.asm

--- src/data/unknown/C3E84E.asm
+++
@@ -1,6 +0,0 @@
-
-;Usage unknown .Only usage seems to be at DRAW_TALL_TEXT_TILE, where these values are passed via X register
-UNKNOWN_C3E84E:
-	.WORD $0130, $0131, $0132, $0133, $0134, $0135, $0136, $0137, $0138, $0000
-UNKNOWN_C3E862:
-	.WORD $0140, $0141, $0142, $0143, $0144, $0145, $0146, $0147, $0148
