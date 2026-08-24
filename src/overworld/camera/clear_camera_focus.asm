46d45d019a2b275098b8a7fb8fd433dd88d074a0
lilliththecreative <111915545+lilliththecreative@users.noreply.github.com>
2026-08-24 15:01:13 -0500

    Update clear_camera_focus.asm

--- src/overworld/camera/clear_camera_focus.asm
+++ src/overworld/camera/clear_camera_focus.asm
@@ -2,6 +2,6 @@
 CLEAR_CAMERA_FOCUS:
 	BEGIN_C_FUNCTION_FAR
 	END_STACK_VARS
-	STZ GAME_STATE + game_state::unknown90
-	STZ GAME_STATE + game_state::unknownB0
+	STZ GAME_STATE + game_state::leader_has_moved
+	STZ GAME_STATE + game_state::camera_mode
 	END_C_FUNCTION
