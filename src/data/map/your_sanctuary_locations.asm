1a71cb218afcd8fd8bb9278faa9da908250d3091
lilliththecreative <111915545+lilliththecreative@users.noreply.github.com>
2026-08-29 09:59:49 -0500

    Update and rename C4DE78.asm to your_sanctuary_locations.asm

---
+++ src/data/map/your_sanctuary_locations.asm
@@ -0,0 +1,10 @@
+
+YOUR_SANCTUARY_LOCATIONS:
+	.WORD $0097, $0030 ;Giant Step
+	.WORD $0183, $03F5 ;Lilliput Steps
+	.WORD $0023, $01E4 ;Rainy Circle
+	.WORD $0058, $029C ;Milky Well
+	.WORD $01DF, $0208 ;Magnet Hill
+	.WORD $01BD, $030B ;Pink Cloud
+	.WORD $034B, $0258 ;Lumine Hall
+	.WORD $02FE, $04CC ;Fire Spring
