48fe905ce2c76f34af23b44e579111aaa074b4d7
lilliththecreative <111915545+lilliththecreative@users.noreply.github.com>
2026-08-29 09:48:52 -0500

    Update and rename C3E44C.asm to menu_next_label.asm

---
+++ src/data/text/menu_next_label.asm
@@ -0,0 +1,3 @@
+
+MENU_NEXT_LABEL:
+	.BYTE $A4, $A8, $66, $00 ;this is untranslated from mother2, resulting in the nonsense string "tx6"
--- src/data/unknown/C3E44C.asm
+++
@@ -1,3 +0,0 @@
-
-UNKNOWN_C3E44C:
-	.BYTE $A4, $A8, $66, $00
