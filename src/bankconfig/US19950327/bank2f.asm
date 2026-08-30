276c3ea87154df440940ec81a5632cad8e21b826
lilliththecreative <111915545+lilliththecreative@users.noreply.github.com>
2026-08-30 12:42:14 -0500

    Update bank2f.asm

--- src/bankconfig/US19950327/bank2f.asm
+++ src/bankconfig/US19950327/bank2f.asm
@@ -73,7 +73,7 @@
 
 .INCLUDE "data/sprite_grouping_data.asm"
 
-.INCLUDE "data/unknown/EF4A40.asm"
+.INCLUDE "data/sound_stone_orbit_sprite_distances.asm"
 
 LOCALEINCLUDE "text_data/EBATTLE6.ebtxt"
 
